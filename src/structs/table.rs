33c764d7ea88f299d08fb9730d02da0abe130cf1
YuInseo <inseo0121@gmail.com>
2024-04-21 18:25:36 +0900

    feat: added table style struct

--- src/structs/table.rs
+++ src/structs/table.rs
@@ -1,6 +1,7 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 use crate::structs::metadata::Metadata;
+use crate::structs::styles::{Color, Style, TextAlignment};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -11,7 +12,19 @@ pub struct Table {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Row(String);
+pub struct TableStyle{
+    pub(crate) width: Option<usize>,
+    pub(crate) text_style: Option<Style>,
+}
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Cell(String);
+#[serde(rename_all = "camelCase")]
+pub struct Row{
+    pub(crate) table_style: TableStyle
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Cell{
+    pub(crate) table_style: TableStyle
+}
