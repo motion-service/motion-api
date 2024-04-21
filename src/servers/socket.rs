200a5a30065985cb0a4ae77fe4f0e46427a4142d
YuInseo <inseo0121@gmail.com>
2024-04-21 23:28:37 +0900

    feat: edit socket server listener

--- src/servers/socket.rs
+++ src/servers/socket.rs
@@ -10,47 +10,21 @@ use socketioxide::{
 };
 
 use serde_json::Value;
-use serde_json::Value::String;
+
 use tracing::{error, info};
 use crate::AppState;
 
 pub fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
-    let ip_address = std::env::var("IP_ADDRESS").expect("IP_ADDRESS must be set.");
-
-    let uri = {
-        let mut uri = ip_address.clone();
-        uri.push_str("/data");
-        uri
-    };
-
-    let uri = match Uri::from_str(&uri) {
-        Ok(uri) => uri,
-        Err(e) => {
-            error!("Failed to parse URI: {:?}", e);
-            return;
-        }
-    };
-
-    socket.on("join",
-              |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
-
-                  info!("Ready ");
-              },
-    );
 
     socket.on("message",
               |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
-                  let test = data.clone();
-
-                  info!("Received event test: {:?} {:?}", data, bin);
-
-                  Request::builder()
-                      .method(Method::POST)
-                      .uri(uri)
-                      .header("content-type", "application/json")
-                      .body(serde_json::to_vec(&data).unwrap())
-
-              },
+                  socket.join("qwerqwer").expect("TODO: panic message");
+              }
     );
 
+    socket.on("chatting",
+              |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+                  socket.to("qwerqwer").emit("broadcast", "test").expect("TODO: panic message");
+              }
+    );
 }
