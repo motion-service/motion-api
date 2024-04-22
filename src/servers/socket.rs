0c8aa3057cd4a206fa84e3c29490b1a1df8cbfb5
YuInseo <inseo0121@gmail.com>
2024-04-22 14:33:28 +0900

    feat: block test code

--- src/servers/socket.rs
+++ src/servers/socket.rs
@@ -4,6 +4,7 @@ use std::thread;
 use std::time::Duration;
 use axum::extract::{Request, State};
 use axum::http::{Method, Uri};
+use serde::{Deserialize, Serialize};
 use socketioxide::{
     extract::{AckSender, Bin, Data, SocketRef},
     SocketIo,
@@ -13,20 +14,35 @@ use serde_json::Value;
 
 use tracing::{error, info};
 use crate::AppState;
+use crate::db::database::Database;
 
-pub fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TestBlock {
+    value: String
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RoomData {
+    id: String,
+    test_blocks: Vec<TestBlock>
+}
+
+pub async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
 
     socket.on("connection",
-              |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
-                  socket.join("test").unwrap();
-                  socket.emit("Joined","test"). unwrap();
+              |socket: SocketRef, Data::<RoomData>(room), Bin(bin)| async move {
+                  socket.join(room.clone().id).unwrap();
 
+                  let database = Database::init().await;
+
+                  info!("connected to {:?}",  room.id);
               }
     );
 
     socket.on("chatting",
-              |socket: SocketRef, Data::<Value>(data)| {
-                  socket.within("test").emit("broadcast", "test").unwrap();
+              |socket: SocketRef, Data::<String>(id)| {
+                  socket.within(id).emit("broadcast", "test").unwrap();
               }
     );
 }
