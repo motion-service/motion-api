2e19306bc0210ae4f056c9fa94ed9718b4045cb9
YuInseo <inseo0121@gmail.com>
2024-04-25 11:06:42 +0900

    feat: added socket state

--- src/servers/socket.rs
+++ src/servers/socket.rs
@@ -2,8 +2,10 @@ use std::str::FromStr;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-use axum::extract::{Request, State};
+
 use axum::http::{Method, Uri};
+use mongodb::bson::{doc, to_document};
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 use socketioxide::{
     extract::{AckSender, Bin, Data, SocketRef},
@@ -11,38 +13,33 @@ use socketioxide::{
 };
 
 use serde_json::Value;
+use socketioxide::extract::State;
 
 use tracing::{error, info};
 use crate::AppState;
 use crate::db::database::Database;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Ord, PartialOrd, Eq)]
 pub struct TestBlock {
-    value: String
+    #[serde(rename = "_id")]
+    pub(crate) id: Option<ObjectId>,
+    pub(crate) value: String
 }
-
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RoomData {
     id: String,
-    test_blocks: Vec<TestBlock>
+    test_blocks: TestBlock
 }
 
-pub async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+pub async fn on_connect(socket: SocketRef) {
+    info!("socket connected: {}", socket.id);
 
-    socket.on("connection",
-              |socket: SocketRef, Data::<RoomData>(room), Bin(bin)| async move {
-                  socket.join(room.clone().id).unwrap();
+    socket.on(
+        "join",
+        |socket: SocketRef, Data::<String>(room), State(app_state): State<Arc<AppState>>| async move {
 
-                  let database = Database::init().await;
-
-                  info!("connected to {:?}",  room.id);
-              }
-    );
-
-    socket.on("chatting",
-              |socket: SocketRef, Data::<String>(id)| {
-                  socket.within(id).emit("broadcast", "test").unwrap();
-              }
+        },
     );
 }
--- src/servers/web.rs
+++ src/servers/web.rs
@@ -3,12 +3,15 @@ use std::sync::{Arc};
 
 // src/server.rs
 use axum::{routing::get, ServiceExt};
+use axum::handler::Handler;
 use axum::routing::{delete, post};
 use axum_client_ip::SecureClientIpSource;
 use serde_json::Value;
-use socketioxide::extract::SocketRef;
-use socketioxide::SocketIo;
 
+use socketioxide::{
+    extract::{Data, SocketRef, State},
+    SocketIo,
+};
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
 use tower_http::cors::CorsLayer;
@@ -21,7 +24,7 @@ use crate::routes::client::page_route::{bread_crumb, create_page, delete_page, e
 use crate::routes::editor::block_route::{create_block, delete_block, edit_block, load_blocks};
 use crate::routes::editor::style_route::{edit_style, load_style};
 use crate::routes::editor::user_route::{get_guests, get_members};
-use crate::servers::socket::on_connect;
+use crate::servers::socket::{on_connect};
 
 pub struct Server {}
 
@@ -29,53 +32,41 @@ impl Server {
     //TODO check out https://github.com/keyosk/alien-metrics/blob/d8882dafac2cc2993582211960c673c1efa62882/src/main.rs#L92
     pub async fn run(){
         let db = Database::init().await;
-        let (layer, io) = SocketIo::builder().build_layer();
-
-        io.ns("/", on_connect);
 
 
         let app_state = Arc::new(AppState {
             db: db.clone(),
         });
 
+        let (layer, io) = SocketIo::builder().with_state(app_state.clone()).build_layer();
+
+        io.ns("/", on_connect);
         let app = axum::Router::new()
             .route("/", get(|| async { "Hello, World!" }))
-
             .route("/auth/register", post(register_account))
             .route("/auth/login", post(login_account))
             .route("/auth/account/:uuid", get(get_account))
-
             .route("/client/page/load/all/:uuid", post(load_all_page))
-            // .route("/client/page/load/bread_crumb", post(bread_crumb))
-            // .route("/client/page/load/single/:uuid", post(load_page))
             .route("/client/page/create/:uuid", post(create_page))
             .route("/client/page/edit/:id/:title/", post(edit_page))
             .route("/client/page/delete/:uuid", delete(delete_page))
-
             .route("/editor/block/create/:uuid", post(create_block))
             .route("/editor/block/delete/:uuid", delete(delete_block))
             .route("/editor/block/load/:uuid", post(load_blocks))
             .route("/editor/block/style/edit/:page_id/:index/:start/:end", post(edit_style))
             .route("/editor/block/style/load/:page_id/:index/:start/:end", post(load_style))
-
             .route("/editor/users/members", post(get_members))
             .route("/editor/users/guests", post(get_guests))
-
             .route("/editor/option/page/font", post(edit_page))
             .route("/editor/option/page/is_shared", post(get_shared))
-
             .with_state(app_state)
-            .layer(layer)
-            .layer(CookieManagerLayer::new())
-            .layer(CorsLayer::permissive())
             .layer(ServiceBuilder::new()
-                .layer(CorsLayer::permissive())
-            .layer(SecureClientIpSource::ConnectInfo.into_extension()));
-
-        //TODO https://medium.com/intelliconnect-engineering/securing-apis-with-apikey-39d0e22f62dd
-        info!("Starting server");
-
-        //TODO Checkout https://doc.rust-lang.org/rust-by-example/macros/designators.html
+                    .layer(layer)
+                    .layer(CookieManagerLayer::new())
+                    .layer(CorsLayer::permissive())
+                    .layer(CorsLayer::permissive())
+                    .layer(SecureClientIpSource::ConnectInfo.into_extension())
+            );
 
         let listener = tokio::net::TcpListener::bind("0.0.0.0:8090").await.unwrap();
         axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
