c4c63c14d2bfe7dc9a4d31dc6398fd5d460c1031
YuInseo <inseo0121@gmail.com>
2024-04-26 20:24:13 +0900

    feat: added build pattern

--- src/structs/user.rs
+++ src/structs/user.rs
@@ -6,26 +6,102 @@ use crate::structs::account::Account;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct User{
+pub struct User {
     #[serde(rename = "_id")]
     pub(crate) id: ObjectId,
     pub(crate) account: Option<Account>,
+    pub(crate) status: Option<StatusType>,
+    pub(crate) latest_page: Option<ObjectId>,
     pub(crate) ip: Option<String>,
     pub(crate) date: Option<DateTime<Local>>,
 }
 
-impl User{
-    pub fn new(account: Account, ip: String) -> Self{
-        User{
-            id: Default::default(),
-            account: Option::from(account),
-            ip: Option::from(ip),
-            date: Option::from(None)
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UserBuilder {
+    #[serde(rename = "_id")]
+    pub(crate) id: Option<ObjectId>,
+    pub(crate) account: Option<Account>,
+    pub(crate) status: Option<StatusType>,
+    pub(crate) latest_page: Option<ObjectId>,
+    pub(crate) ip: Option<String>,
+    pub(crate) date: Option<DateTime<Local>>,
+}
+
+impl UserBuilder {
+    pub fn new() -> UserBuilder {
+        UserBuilder {
+            id: None,
+            account: None,
+            status: None,
+            latest_page: None,
+            ip: None,
+            date: None,
+        }
+    }
+    pub fn id(mut self, id: Option<ObjectId>) -> UserBuilder {
+        self.id = id;
+        self
+    }
+    pub fn account(mut self, account: Option<Account>) -> UserBuilder {
+        self.account = account;
+        self
+    }
+    pub fn status(mut self, status_type: Option<StatusType>) -> UserBuilder {
+        self.status = status_type;
+        self
+    }
+    pub fn latest_page(mut self, id: Option<ObjectId>) -> UserBuilder {
+        self.latest_page = id;
+        self
+    }
+    pub fn ip(mut self, ip: Option<String>) -> UserBuilder {
+        self.ip = ip;
+        self
+    }
+    pub fn date(mut self, date: Option<DateTime<Local>>) -> UserBuilder {
+        self.date = date;
+        self
+    }
+    pub fn build(self) -> User {
+        let id = if self.id.is_none() {
+            Some(Default::default())
+        } else {
+            self.id
+        };
+
+        User {
+            id: id.to_owned().unwrap(),
+            account: self.account,
+            status: self.status,
+            latest_page: self.latest_page,
+            ip: self.ip,
+            date: self.date,
         }
     }
 }
 
-impl From<Document> for User{
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum StatusType {
+    Online,
+    Offline,
+    Busy,
+}
+
+impl User {
+    pub fn new(account: Account, ip: String) -> Self {
+        User {
+            id: Default::default(),
+            account: Option::from(account),
+            status: None,
+            latest_page: None,
+            ip: Option::from(ip),
+            date: Option::from(None),
+        }
+    }
+}
+
+impl From<Document> for User {
     fn from(doc: Document) -> Self {
         let user_id = doc.get("_id").unwrap();
 
@@ -37,9 +113,11 @@ impl From<Document> for User{
 
         let account = Account::new(Option::from(account_nickname), Option::from(account_password));
 
-        User{
+        User {
             id: user_id.as_object_id().unwrap(),
             account: Option::from(account),
+            status: None,
+            latest_page: None,
             ip: Option::from(ip),
             date: None,
         }
